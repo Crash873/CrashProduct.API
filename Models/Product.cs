a60d345be1aa1f3bb4d59b22b4dea21203df0dcd
Rodrigo <rodrigo_web2@hotmail.com>
2020-01-10 19:04:09 -0300

    First commit

---
+++ Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CrashProduto.API.Models
+{
+    public class Product
+    {
+        [Required]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+    }
+}
